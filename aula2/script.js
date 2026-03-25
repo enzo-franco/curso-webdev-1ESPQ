68330e20ebfc8b1f749fc6773d745997bf19760d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 22:31:55 -0300

    conteudo

---
+++ aula2/script.js
@@ -0,0 +1,90 @@
+// let idade = 25
+// let temCarteira = true
+
+// //AND (&&)
+// if(idade >= 18 && temCarteira){
+//     console.log("Pode dirigir")
+// }
+
+// //OR (||)
+// if(idade < 18 || !temCarteira){
+//     console.log("Não pode dirigir")
+// }
+
+// //NOT (!) - INVERTE VALOR
+// if(!temCarteira){
+//     console.log("Precisa de carteira")
+// }
+
+// let tarefaConcluida = true;
+
+// if (tarefaConcluida) {
+//   console.log("A tarefa está concluída!");
+// }
+
+// let prioridade = 2; // 1: baixa, 2: média, 3: alta
+
+// if (prioridade === 3) {
+//   console.log("Prioridade Alta");
+// }else if (prioridade ===2){
+//     console.log("Prioridade média")
+// }
+// else if (prioridade ===1){
+//     console.log("Prioridade média")
+// }
+
+// else {
+//   console.log("Prioridade desconhecida");
+// }
+
+// let diaSemana = new Date().getDay()
+// console.log(diaSemana)
+
+// switch (diaSemana) {
+//     case 0:
+//         console.log("Domingo")
+//         break;
+//     case 1:
+//         console.log("Segunda")
+//         break;
+//     case 2:
+//         console.log("Terça")
+//         break;
+//     case 3:
+//         console.log("Quarta")
+//         break;
+//     case 4:
+//         console.log("Quinta")
+//         break;
+//     case 5:
+//         console.log("Sexta")
+//         break;
+//     case 6:
+//         console.log("Sábado")
+//         break;
+
+//     default:
+//         console.log("Não é um dia válido")
+//         break;
+// }
+
+// for (let i = 0;i <= 5;i++) {
+//     console.log("Contagem", i)
+// }
+
+// let contador = 0;
+// while (contador < 5) {
+//   console.log("Contagem:", contador);
+//   contador++;
+// }
+
+// let numero = 5;
+// do {
+//   console.log("Número é:", numero);
+//   numero--;
+// } while (numero > 0);
+
+//                  EXERCÍCIOS 
+
+//EX 1
+
